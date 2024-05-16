6ee483012283f13daee673c34f24173ea2aadd5b
RASEC Gonzalez <al052899@uacam.mx>
2024-05-16 16:32:28 -0600

    Update RFCInput.vue

--- vue-test-demo/src/components/RFCInput.vue
+++ vue-test-demo/src/components/RFCInput.vue
@@ -1,5 +1,19 @@
 <template setup>
-  import { ref, watch } from 'vue'
 
   <div>Esto será el componente RFCInput</div>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+cont props = defineProps({
+  RFC: String
+});
+
+const inputValue = ref(props.RFC || );´
+const isValid = ref(true);
+
+function validateRFC() {
+  const rfcPattern = /^[A-ZÑ&]{3,4}\[6](?:[A-Z\d]{3}(?\d{9]{3})?$/i;
+  
+}
+</script>
