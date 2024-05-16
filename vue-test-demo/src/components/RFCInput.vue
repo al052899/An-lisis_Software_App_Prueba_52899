5a3dca61632c0d3dd3c31247f5410e1b1b9ae368
RASEC Gonzalez <al052899@uacam.mx>
2024-05-16 16:40:17 -0600

    a ver

--- vue-test-demo/src/components/RFCInput.vue
+++ vue-test-demo/src/components/RFCInput.vue
@@ -3,32 +3,34 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
-cont props = defineProps({
+import { ref } from 'vue'
+const props = defineProps({
   RFC: String
-});
+})
 
-const inputValue = ref(props.RFC || );´
-const isValid = ref(true);
+const inputValue = ref(props.RFC || '')
+
+const isValid = ref(true)
 
 function validateRFC() {
-  const rfcPattern = /^[A-ZÑ&]{3,4}\[6](?:[A-Z\d]{3}(?\d{9]{3})?$/i;
-  isValid.value = rfcPattern.test(inputValue.value);
-  console.log(isValid.value);
-
+  const rfcPattern = /^[A-ZÑ&]{3,4}\d[6](?:[A-Z\d]{3})?$/i
+  isValid.value = rfcPattern.test(inputValue.value)
+  console.log(isValid.value)
 }
 
-watch(() => props.RFC, (newValue) => {
-  inputValue.value = newValue;
-  validateRFC();
-});
+watch(
+  () => props.RFC,
+  (newValue) => {
+    inputValue.value = newValue
+    validateRFC()
+  }
+)
 
-watch(inputValue, validateRFC);
+watch(inputValue, validateRFC)
 </script>
 
 <style scoped>
 .invalid {
   color: red;
 }
-
 </style>
