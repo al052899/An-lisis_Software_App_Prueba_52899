068b099cbe77f18fc68af5b2d175c86458c95f41
RASEC Gonzalez <al052899@uacam.mx>
2024-05-16 16:15:46 -0600

    update

--- vue-test-demo/src/App.vue
+++ vue-test-demo/src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import RFCInput from './components/RFCInput.vue'
 </script>
 
 <template>
@@ -8,40 +7,7 @@ import TheWelcome from './components/TheWelcome.vue'
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <RFCInput />
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ vue-test-demo/src/components/RFCInput.vue
@@ -0,0 +1,5 @@
+<template setup>
+  import { ref, watch } from 'vue'
+
+  <div>Esto será el componente RFCInput</div>
+</template>
